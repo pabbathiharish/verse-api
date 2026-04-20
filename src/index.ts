ac19f27ce8131d0ddfdb4396acf72a91585f9536
harish chandra <harishpabbathi@gmail.com>
2026-04-20 19:46:35 +0530

    - Removed sample methods like TASK

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,5 @@
 import { fromHono } from "chanfana";
 import { Hono } from "hono";
-import { TaskCreate } from "./endpoints/taskCreate";
-import { TaskDelete } from "./endpoints/taskDelete";
-import { TaskFetch } from "./endpoints/taskFetch";
-import { TaskList } from "./endpoints/taskList";
 import { CreateVerse } from "./endpoints/createVerse";
 import { GetVerses } from "./endpoints/getVerses";
 
@@ -16,11 +12,6 @@ const openapi = fromHono(app, {
 });
 
 // Register OpenAPI endpoints
-openapi.get("/api/tasks", TaskList);
-openapi.post("/api/tasks", TaskCreate);
-openapi.get("/api/tasks/:taskSlug", TaskFetch);
-openapi.delete("/api/tasks/:taskSlug", TaskDelete);
-
 openapi.post("/api/verses", CreateVerse);
 openapi.get("/api/verses/feed", GetVerses);
 
