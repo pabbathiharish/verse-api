c6157b3d6069e7d78d929afc1756dd9776debbab
pabbathiharish <harishpabbathi@gmail.com>
2026-05-26 15:36:29 +0530

    Create firebaseConfig.ts
    - added new firebaseconfig resolver

---
+++ src/utils/firebaseConfig.ts
@@ -0,0 +1,54 @@
+import type { Env }
+	from "../types";
+
+export interface FirebaseProjectConfig {
+
+	projectId: string;
+
+	clientEmail: string;
+
+	privateKey: string;
+}
+
+export function getFirebaseConfig(
+	env: Env,
+	project: string
+): FirebaseProjectConfig {
+
+	switch (project) {
+
+		case "santa":
+
+			return {
+
+				projectId:
+					env.SANTA_FIREBASE_PROJECT_ID,
+
+				clientEmail:
+					env.SANTA_FIREBASE_CLIENT_EMAIL,
+
+				privateKey:
+					env.SANTA_FIREBASE_PRIVATE_KEY
+			};
+
+		case "portuguese":
+
+			return {
+
+				projectId:
+					env.PORTUGUESE_FIREBASE_PROJECT_ID,
+
+				clientEmail:
+					env.PORTUGUESE_FIREBASE_CLIENT_EMAIL,
+
+				privateKey:
+					env.PORTUGUESE_FIREBASE_PRIVATE_KEY
+			};
+
+		default:
+
+			throw new Error(
+				`Unknown firebase project: ${project}`
+			);
+	}
+}
