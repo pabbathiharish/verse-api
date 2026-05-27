4fa2b8fc8b1599501f08d6a6bd0176fc1c355563
harish chandra <harishpabbathi@gmail.com>
2026-05-27 14:29:29 +0530

    - added more topic languages

--- src/services/push.ts
+++ src/services/push.ts
@@ -11,72 +11,12 @@ import {
 	getFirebaseConfig
 } from "../utils/firebaseConfig";
 
-const TOPIC_LANGUAGE_MAP = [
+import {
+	topicLanguageMap
+} from "../utils/topicLanguageMap";
 
-	{
-		topic: "daily_verse_image_zu",
-		language: "eng",
-		project: "portuguese",
-		deeplinkSchema: "hcbibleapps"
-	},
 
-	{
-		topic: "daily_verse_image_fr",
-		language: "fr",
-		project: "santa",
-		deeplinkSchema: "hcbibleapps"
 
-	},
-
-	{
-		topic: "daily_verse_image_es",
-		language: "es",
-		project: "santa",
-		deeplinkSchema: "hcbibleapps"
-	},
-
-	{
-		topic: "daily_verse_image_se",
-		language: "eng",
-		project: "portuguese",
-		deeplinkSchema: "hcbibleapps"
-	},
-
-	{
-		topic: "daily_verse_image_hi",
-		language: "hi",
-		project: "santa",
-		deeplinkSchema: "hcbibleapps"
-	},
-
-	{
-		topic: "daily_verse_image_te",
-		language: "te",
-		project: "santa",
-		deeplinkSchema: "hcbibleapps"
-	},
-
-	{
-		topic: "daily_verse_image_ta",
-		language: "ta",
-		project: "santa",
-		deeplinkSchema: "hcbibleapps"
-	},
-
-	{
-		topic: "daily_verse_image_ko",
-		language: "ko",
-		project: "portuguese",
-		deeplinkSchema: "hcbibleapps"
-	},
-
-	{
-		topic: "daily_verse_image_ar",
-		language: "ar",
-		project: "portuguese",
-		deeplinkSchema: "hcbibleapps"
-	}
-];
 
 export async function sendVersePush(
 	env: Env,
@@ -89,7 +29,7 @@ export async function sendVersePush(
 
 	for (
 		const item
-		of TOPIC_LANGUAGE_MAP
+		of topicLanguageMap
 	) {
 
 
---
+++ src/utils/topicLanguageMap.ts
@@ -0,0 +1,92 @@
+
+export const topicLanguageMap = [
+
+	{
+		topic: "daily_verse_image_zu",
+		language: "eng",
+		project: "portuguese",
+		deeplinkSchema: "hcbibleapps"
+	},
+
+	{
+		topic: "daily_verse_image_fr",
+		language: "fr",
+		project: "santa",
+		deeplinkSchema: "hcbibleapps"
+
+	},
+
+	{
+		topic: "daily_verse_image_es",
+		language: "es",
+		project: "santa",
+		deeplinkSchema: "hcbibleapps"
+	},
+
+	{
+		topic: "daily_verse_image_se",
+		language: "eng",
+		project: "portuguese",
+		deeplinkSchema: "hcbibleapps"
+	},
+
+	{
+		topic: "daily_verse_image_hi",
+		language: "hi",
+		project: "santa",
+		deeplinkSchema: "hcbibleapps"
+	},
+
+	{
+		topic: "daily_verse_image_te",
+		language: "te",
+		project: "santa",
+		deeplinkSchema: "hcbibleapps"
+	},
+
+	{
+		topic: "daily_verse_image_ta",
+		language: "ta",
+		project: "santa",
+		deeplinkSchema: "hcbibleapps"
+	},
+
+	{
+		topic: "daily_verse_image_ko",
+		language: "ko",
+		project: "portuguese",
+		deeplinkSchema: "hcbibleapps"
+	},
+
+	{
+		topic: "daily_verse_image_ar",
+		language: "ar",
+		project: "portuguese",
+		deeplinkSchema: "hcbibleapps"
+	},
+	{
+		topic: "daily_verse_image_sw",
+		language: "eng",
+		project: "santa",
+		deeplinkSchema: "hcbibleapps"
+	},
+	{
+		topic: "daily_verse_image_st",
+		language: "eng",
+		project: "portuguese",
+		deeplinkSchema: "hcbibleapps"
+	},
+	{
+		topic: "daily_verse_image_ml",
+		language: "eng",
+		project: "santa",
+		deeplinkSchema: "hcbibleapps"
+	},
+	{
+		topic: "daily_verse_image_kn",
+		language: "eng",
+		project: "santa",
+		deeplinkSchema: "hcbibleapps"
+	}
+	
+];
